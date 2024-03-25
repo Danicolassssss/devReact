ef802de9b6340591ac3ee8031e8d7b2f43bd9538
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-03-25 13:19:14 +0100

    DÉTECTER LE TYPE DE TÉLÉPHONE

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import { Text,View,Image } from "react-native";
+import { Text,View,Image,Platform } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import { Child } from "./components/Child";
 
 
 export default function App() {
-  const [age, setAge] = useState(0);
-  function hello(name){
-    setAge(age+1);
-  }
-  console.log(age);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{backgroundColor:"red", flex:1}}>
-        <Child onPress={hello} />
+        
+        {Platform.OS === "ios" ? <Text>IOS</Text> : <Text>Android</Text>}
       </SafeAreaView>
     </SafeAreaProvider>
     
