b6608d227e0789ecf38b2b2f6f75eba31392d49c
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-03-25 10:18:08 +0100

    LES FLEX BOX

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { Text,View } from "react-native";
 import { User } from "./components/User";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+import { FlexDemo } from "./components/FlexDemo/FlexDemo";
 
 export const s = StyleSheet.create({
   square:{
@@ -19,13 +20,7 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{backgroundColor:"red", flex:1}}>
-        <View style={s.square}>
-          <Text style={s.text}>Hello World</Text>
-          <Text style={s.text}>Hello World</Text>
-          <User/>
-          <Text style={s.text}>Hello World</Text>
-      </View>
-
+        <FlexDemo />
       </SafeAreaView>
     </SafeAreaProvider>
     
--- components/FlexDemo/FlexDemo.js
+++ components/FlexDemo/FlexDemo.js
@@ -1,9 +1,16 @@
 import {s} from "./FlexDemo.style"
 import {View} from "react-native"
+import {Text} from "react-native"
 
 export function FlexDemo(){
     return(
         <View style={s.container}>
+            <View style={s.box1}>
+            </View>
+            <View style={s.box2}>
+            </View>
+            <View style={s.box3}>
+            </View> 
         </View>
     )
 }
