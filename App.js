58f453c27ac3cde605476216adcc74ddbdc2ac3c
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-03-25 10:00:21 +0100

    APPLIQUER DU STYLE

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import { Text,View } from "react-native";
 import { User } from "./components/User";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import {s} from "./App.style"
-import { FlexDemo } from "./components/FlexDemo/FlexDemo";
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  square:{
+    width:100,
+    height:100,
+    backgroundColor:"blue"
+  },
+  text:{
+    color:"white",
+    fontSize:30
+  }
+})
 
 export default function App() {
   return (
-    <View>    
-      <Text style={s.text}>Hello World</Text>
-      <Text style={s.text}>Hello World</Text>
-      <Text style={s.text}>Hello World</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{backgroundColor:"red", flex:1}}>
+        <View style={s.square}>
+          <Text style={s.text}>Hello World</Text>
+          <Text style={s.text}>Hello World</Text>
+          <User/>
+          <Text style={s.text}>Hello World</Text>
+      </View>
 
+      </SafeAreaView>
+    </SafeAreaProvider>
+    
   );
 }
 
